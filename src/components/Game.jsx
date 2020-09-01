ce4d742627c8d48e1d515827c1c6af1af12fd803
Madhura <madhura.bhujbal@gmail.com>
2020-09-01 16:11:57 +0100

    Import customized css file for Game and apply style to main div tag

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
 import Board from "./Board";
+import gameStyle from "../gameStyle.css";
 
 export default class Game extends Component {
   render() {
     return (
-      <div className="game">
+      <div className="game" style={gameStyle}>
         <div className="game-board">
           <Board />
         </div>
