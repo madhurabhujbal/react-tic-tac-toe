e1e0e386a0525caed52b1354c598a399bdca672b
Madhura <madhura.bhujbal@gmail.com>
2020-09-01 15:44:10 +0100

    Correct the Component class in Board component

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Square from "./Square";
 
-export default class Board extends Component {
+export default class Board extends React.Component {
   renderSquare(i) {
     return <Square />;
   }
