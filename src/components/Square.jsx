221fbce51a4ca0780b2e505628a9cc8039572d76
Madhura <madhura.bhujbal@gmail.com>
2020-09-02 10:21:50 +0100

    Add constructor to Square class to initialize the state of squares

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 
 export default class Square extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { value: null };
+  }
   render() {
     return (
       <button
