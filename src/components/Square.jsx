90f6dfebd637db7728e2d8531a87a20fea7f6fba
Madhura <madhura.bhujbal@gmail.com>
2020-09-02 16:41:09 +0100

    Convert Square class into a function component and export it

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 
-export default class Square extends React.Component {
-  render() {
-    return (
-      <button className="square" onClick={() => this.props.onClick()}>
-        {" "}
-        {this.props.value}{" "}
-      </button>
-    );
-  }
+function Square() {
+  return (
+    <button className="square" onClick={() => this.props.onClick()}>
+      {" "}
+      {this.props.value}{" "}
+    </button>
+  );
 }
+
+export default Square;
