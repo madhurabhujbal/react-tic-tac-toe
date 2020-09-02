b4f61795732ca156eae1eccb688e46773a117529
Madhura <madhura.bhujbal@gmail.com>
2020-09-02 15:54:36 +0100

    Delete the constructor in Square component

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,10 +1,6 @@
 import React from "react";
 
 export default class Square extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { value: null };
-  }
   render() {
     return (
       <button className="square" onClick={() => this.props.onClick()}>
