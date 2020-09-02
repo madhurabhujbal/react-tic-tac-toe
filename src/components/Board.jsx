cb58a28e73420fcd1a50d9e24fe1300792e0a6d0
Madhura <madhura.bhujbal@gmail.com>
2020-09-02 15:35:57 +0100

    Pass the onClick event as props to Square component

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -10,7 +10,12 @@ export default class Board extends React.Component {
     };
   }
   renderSquare(i) {
-    return <Square value={this.state.squares[i]} />;
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    );
   }
 
   render() {
