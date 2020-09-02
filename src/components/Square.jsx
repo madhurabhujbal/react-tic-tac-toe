569eee8487289608ebc8db69c3836e22548146bb
Madhura <madhura.bhujbal@gmail.com>
2020-09-02 15:53:00 +0100

    Change onClick state of squares from parent Board component instead of from state object

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -7,7 +7,7 @@ export default class Square extends React.Component {
   }
   render() {
     return (
-      <button className="square" onClick={() => this.setState({ value: "X" })}>
+      <button className="square" onClick={() => this.props.onClick()}>
         {" "}
         {this.props.value}{" "}
       </button>
