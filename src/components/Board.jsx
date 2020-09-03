604aec680c2af0056fb37546bc7dc91c414916a3
Madhura <madhura.bhujbal@gmail.com>
2020-09-03 10:58:53 +0100

    Call function to calculate winner and change the status accordingly

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -28,7 +28,15 @@ export default class Board extends React.Component {
   }
 
   render() {
-    const status = "Next player : " + (this.state.xIsNext ? "X" : "O");
+    const winner = calculateWinner(this.state.squares);
+    // const status = "Next player : " + (this.state.xIsNext ? "X" : "O");
+    let status;
+    if (winner) {
+      status = "Winner " + winner;
+    } else {
+      status = "Next player : " + (this.state.xIsNext ? "X" : "O");
+    }
+
     return (
       <div>
         <div className="status"> {status} </div>
