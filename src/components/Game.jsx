7543278e1e94e5610ba8f74b766eff8bdc82f171
Madhura <madhura.bhujbal@gmail.com>
2020-09-04 14:49:12 +0100

    Rearrange the div tags to maintain display of game board

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -53,15 +53,15 @@ export default class Game extends Component {
     }
     return (
       <div className="game" style={gameStyle}>
-        <div className="game-board">
-          <Board
-            squares={current.squares}
-            onClick={(i) => this.handleClick(i)}
-          />
-        </div>
         <div className="game-info">
           <div> {status} </div>
           <ol></ol>
+          <div className="game-board">
+            <Board
+              squares={current.squares}
+              onClick={(i) => this.handleClick(i)}
+            />
+          </div>
         </div>
       </div>
     );
