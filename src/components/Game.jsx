2c5d19229f5c6eb08b8939cb37aa0f15be72580b
Madhura <madhura.bhujbal@gmail.com>
2020-09-04 10:21:44 +0100

    Add previous state of squares in constructor of main Game component

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -3,6 +3,18 @@ import Board from "./Board";
 import gameStyle from "../gameStyle.css";
 
 export default class Game extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      history: [
+        {
+          squares: Array(9).fill(null),
+        },
+      ],
+      xIsNext: true,
+    };
+  }
+
   render() {
     return (
       <div className="game" style={gameStyle}>
